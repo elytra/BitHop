0523a465cdcd6250f7e602a13f42a6d74e1723b3
Meredith Espinosa <goclonefilms@gmail.com>
2018-04-01 19:02:56 -0700

    trying to kotlin out the bithop

--- src/main/java/com/elytradev/bithop/tile/TileEntityBitHop.kt
+++ src/main/java/com/elytradev/bithop/tile/TileEntityBitHop.kt
@@ -8,11 +8,13 @@ import com.elytradev.concrete.inventory.ValidatedInventoryView
 import com.elytradev.concrete.inventory.ValidatedItemHandlerView
 import net.minecraft.block.BlockChest
 import net.minecraft.inventory.IInventory
+import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.tileentity.TileEntityChest
 import net.minecraft.tileentity.TileEntityHopper
 import net.minecraft.util.EntitySelectors
 import net.minecraft.util.EnumFacing
+import net.minecraft.util.ITickable
 import net.minecraft.util.math.AxisAlignedBB
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
@@ -21,12 +23,13 @@ import net.minecraftforge.items.CapabilityItemHandler
 
 val CAPACITY = 5
 
-class TileEntityBitHop : TileEntity(), IContainerInventoryHolder {
+class TileEntityBitHop : TileEntity(), IContainerInventoryHolder, ITickable {
     val inv = ConcreteItemStorage(CAPACITY).withName("${ModBlocks.BITHOP.unlocalizedName}.name")
     var direction: EnumFacing? = null
+    var cooldown = 8;
+    var maxCooldown = 8;
     init {
         inv.listen{markDirty()}
-        inv.listen{pushItems()}
     }
 
     override fun getContainerInventory() = ValidatedInventoryView(inv)
@@ -45,8 +48,30 @@ class TileEntityBitHop : TileEntity(), IContainerInventoryHolder {
         getInventoryAtPosition(getWorld(), pos.add(BlockBitHop.getFacing(blockMetadata).directionVec), false)
     }
 
+    override fun update() {
+        if(!world.isRemote) {
+            if (cooldown > 0) {
+                cooldown--
+                this.markDirty()
+                return
+            }
+            val slot = getFirstFullSlot()
+            if (slot != -1) {
+                var tile = world.getTileEntity(getPos().offset(direction))
+                if (tile == null || !tile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, direction?.getOpposite())) return
+                var cap = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, direction?.opposite)
+                var extract = inv.extractItem(slot, 1, true)
+                if (!extract.isEmpty) {
+                    var insert = cap?.insertItem(1, extract, false)
+                    if (!insert!!.isEmpty) inv.extractItem(slot, 1, false)
+                }
+            }
+            cooldown = maxCooldown
+        }
+    }
+
     override fun hasCapability(capability: Capability<*>, facing: EnumFacing?): Boolean {
-        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) return true;
+        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) return true
         return super.hasCapability(capability, facing)
     }
 
