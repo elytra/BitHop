ee5de3aa39f7f22f3add9f09322fe264c3b3d9b0
Meredith Espinosa <goclonefilms@gmail.com>
2018-04-01 20:53:41 -0700

    trim things down

--- src/main/java/com/elytradev/bithop/tile/TileEntityBitHop.kt
+++ src/main/java/com/elytradev/bithop/tile/TileEntityBitHop.kt
@@ -6,18 +6,10 @@ import com.elytradev.concrete.inventory.ConcreteItemStorage
 import com.elytradev.concrete.inventory.IContainerInventoryHolder
 import com.elytradev.concrete.inventory.ValidatedInventoryView
 import com.elytradev.concrete.inventory.ValidatedItemHandlerView
-import net.minecraft.block.BlockChest
-import net.minecraft.inventory.IInventory
 import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
-import net.minecraft.tileentity.TileEntityChest
-import net.minecraft.tileentity.TileEntityHopper
-import net.minecraft.util.EntitySelectors
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.ITickable
-import net.minecraft.util.math.AxisAlignedBB
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.World
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.items.CapabilityItemHandler
 import net.minecraftforge.items.IItemHandler
@@ -26,30 +18,16 @@ val CAPACITY = 5
 
 class TileEntityBitHop : TileEntity(), IContainerInventoryHolder, ITickable {
     val inv = ConcreteItemStorage(CAPACITY).withName("${ModBlocks.BITHOP.unlocalizedName}.name")
-    var direction: EnumFacing? = null
-    var cooldown = 8;
-    var maxCooldown = 8;
+    var cooldown = 8
     init {
         inv.listen{markDirty()}
     }
 
     override fun getContainerInventory() = ValidatedInventoryView(inv)
 
-
-    fun pushItems() {
-        val slot = getFirstFullSlot()
-        if (slot != -1) {
-
-        }
-    }
-
     private fun getFirstFullSlot(): Int = (0 until CAPACITY).firstOrNull{inv.getCanExtract(it)} ?: -1
 
     private fun getFirstEmptylot(slotTotal: Int, cap: IItemHandler, test: ItemStack): Int = (0 until slotTotal).firstOrNull{cap.insertItem(it, test, true).isEmpty()} ?: -1
-
-    private fun getInventoryForHopperTransfer() {
-        getInventoryAtPosition(getWorld(), pos.add(BlockBitHop.getFacing(blockMetadata).directionVec), false)
-    }
 
     override fun update() {
         if(!world.isRemote) {
@@ -60,20 +38,18 @@ class TileEntityBitHop : TileEntity(), IContainerInventoryHolder, ITickable {
             }
             val slot = getFirstFullSlot()
             if (slot != -1) {
-                var tile = world.getTileEntity(getPos().offset(BlockBitHop.getFacing(blockMetadata)))
-                if (tile == null || !tile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, BlockBitHop.getFacing(blockMetadata).getOpposite())) return
-                var cap = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, BlockBitHop.getFacing(blockMetadata).getOpposite())
-                var extract = inv.extractItem(slot, 1, true)
+                val tile = world.getTileEntity(getPos().offset(BlockBitHop.getFacing(blockMetadata))) ?: return
+                val cap = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, BlockBitHop.getFacing(blockMetadata).getOpposite())!!
+                val extract = inv.extractItem(slot, 1, true)
                 if (!extract.isEmpty) {
-                    var slotTotal = cap!!.slots
-                    var insertSlot = getFirstEmptylot(slotTotal, cap, extract)
+                    val insertSlot = getFirstEmptylot(cap.slots, cap, extract)
                     if (insertSlot != -1) {
-                        var insert = cap.insertItem(insertSlot, extract, false)
+                        val insert = cap.insertItem(insertSlot, extract, false)
                         if (insert.isEmpty) inv.extractItem(slot, 1, false)
                     }
                 }
             }
-            cooldown = maxCooldown
+            cooldown = 8
         }
     }
 
@@ -90,30 +66,4 @@ class TileEntityBitHop : TileEntity(), IContainerInventoryHolder, ITickable {
             else -> super.getCapability(capability, facing)
         }
     }
-}
-
-fun getInventoryAtPosition(worldIn: World, pos: BlockPos, scanEntities: Boolean = false): IInventory? {
-    val state = worldIn.getBlockState(pos)
-    val block = state.block
-    if (block.hasTileEntity(state)) {
-        val te = worldIn.getTileEntity(pos)
-        if (te is IInventory) {
-            return if (te is TileEntityChest && block is BlockChest)
-                block.getContainer(worldIn, pos, true) ?: te
-            else
-                te
-        }
-    }
-
-    return if (scanEntities) {
-        val inRange = worldIn.getEntitiesInAABBexcluding(null,
-                AxisAlignedBB(pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble(),
-                        pos.x + 1.0, pos.y + 1.0, pos.z + 1.0),
-                EntitySelectors.HAS_INVENTORY)
-
-        if (inRange.isNotEmpty())
-            inRange[worldIn.rand.nextInt(inRange.size)] as IInventory
-        else
-            null
-    } else null
 }
