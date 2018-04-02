1a3e62d6aa672fc2d4acc3fe169c7488ffddcbbf
Meredith Espinosa <goclonefilms@gmail.com>
2018-04-01 21:45:02 -0700

    more tweaking

--- src/main/java/com/elytradev/bithop/tile/TileEntityBitHop.kt
+++ src/main/java/com/elytradev/bithop/tile/TileEntityBitHop.kt
@@ -14,12 +14,14 @@ import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.items.CapabilityItemHandler
 import net.minecraftforge.items.IItemHandler
 
-val CAPACITY = 5
 
 class TileEntityBitHop : TileEntity(), IContainerInventoryHolder, ITickable {
+    companion object {
+        const val MAX_COOLDOWN = 8
+        const val CAPACITY = 5
+    }
     val inv = ConcreteItemStorage(CAPACITY).withName("${ModBlocks.BITHOP.unlocalizedName}.name")
     var cooldown = 8
-    val MAX_COOLDOWN = 8
     init {
         inv.listen{markDirty()}
     }
@@ -28,7 +30,7 @@ class TileEntityBitHop : TileEntity(), IContainerInventoryHolder, ITickable {
 
     private fun getFirstFullSlot(): Int = (0 until CAPACITY).firstOrNull{inv.getCanExtract(it)} ?: -1
 
-    private fun getFirstEmptylot(slotTotal: Int, cap: IItemHandler, test: ItemStack): Int = (0 until slotTotal).firstOrNull{cap.insertItem(it, test, true).isEmpty()} ?: -1
+    private fun getFirstEmptySlot(slotTotal: Int, cap: IItemHandler, test: ItemStack): Int = (0 until slotTotal).firstOrNull{cap.insertItem(it, test, true).isEmpty()} ?: -1
 
     override fun update() {
         if(!world.isRemote) {
@@ -43,7 +45,7 @@ class TileEntityBitHop : TileEntity(), IContainerInventoryHolder, ITickable {
                 val cap = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, BlockBitHop.getFacing(blockMetadata).getOpposite())!!
                 val extract = inv.extractItem(slot, 1, true)
                 if (!extract.isEmpty) {
-                    val insertSlot = getFirstEmptylot(cap.slots, cap, extract)
+                    val insertSlot = getFirstEmptySlot(cap.slots, cap, extract)
                     if (insertSlot != -1) {
                         val insert = cap.insertItem(insertSlot, extract, false)
                         if (insert.isEmpty) inv.extractItem(slot, 1, false)
